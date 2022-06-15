c2bce2786705d7ef0b0ac5f0a4aa8f4d5e402666
hijeong_ <gmlwjdchl82@naver.com>
2022-06-15 09:59:03 +0900

    props

--- src/App.js
+++ src/App.js
@@ -5,19 +5,34 @@ import {
   FnComponent_1,
   FnComponent_2,
 } from "./components/2_component/FnComponent";
+import { FoodMenu } from "./components/2_component/FoodMenu";
 
 const App = () => {
   return (
     <>
       {/* <TagRule /> */}
-      {/* 1일차 */}
+      {/* ==========1일차========== */}
 
       {/* <FnComponent /> */}
       {/* <FnComponent_1 />
       <FnComponent_2 /> */}
 
       {/* <ClassComponent /> */}
-      {/* 2일차 */}
+      {/* =======2일차========== */}
+
+      {/* // props
+      // 매개변수와 비슷
+      // <component 프롭스명="값"/> 
+      오브젝트 형태로 전달됨(중괄호까지)=> 매개변수를 쓸때는 중괄호를 빼야함
+      */}
+      <h3>아침</h3>
+      <FoodMenu menuName="계란밥" subMenu="된장국" />
+
+      <h3>점심</h3>
+      <FoodMenu menuName="우동" subMenu="김치" />
+
+      <h3>저녁</h3>
+      <FoodMenu menuName="김치볶음밥" subMenu="계란국" />
     </>
   );
 };
--- src/components/2_component/FoodMenu.js
+++ src/components/2_component/FoodMenu.js
@@ -1,3 +1,22 @@
-export const FoodMenu = () => {
-  return <h3>오늘의 메뉴 : 텐동</h3>;
+import "../../styles/style.css";
+// from은 component를 가져올때 사용
+// 파일명은 직접 작성
+
+export const FoodMenu = ({ menuName, subMenu }) => {
+  console.log(menuName);
+  console.log(subMenu);
+  return (
+    <div>
+      오늘의 메뉴 : <i className="menu">{menuName}</i>
+      <br></br>
+      사이드 메뉴 : <i className="side">{subMenu}</i>
+    </div>
+  );
 };
+// ({매개변수 명})
+// 프롭스는 오브젝트 형태로 가져옴 (props) => {menuName="값"}
+// 오브젝트는 {}안에서 ,로 구분됨
+// 중괄호 빼줘야함
+// return 안쪽은 jsx문법, jsx 안쪽에 변수를 사용할때는 중괄호{}로 감싸주기
+
+// 리액트에서 클래스(css)사용할때 className으로 사용
